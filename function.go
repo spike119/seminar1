b4e6484c3f2dfcf4f19503416096a3d362a78faf
spike119 <whdals0119@naver.com>
2021-03-21 16:17:09 +0900

    * go 파일을 상위 폴더로 옮김

--- function.go
+++ function.go
@@ -6,17 +6,32 @@ func sum(n1 int, n2 int) int {
 	return n1 + n2
 }
 
-func main(){
+func call_by_value(a int) {
+	a = 10
+}
+
+func call_by_ref(a *int) {
+	*a = 10
+}
+
+func main() {
 	var n1 int
 	n1 = 10
-	
+
 	var n2 = 20
-	
+
 	n3 := 30
-	n4 := 40	
+	n4 := 40
 
 	fmt.Println("sum : ", sum(n1, n2))
 
 	result := sum(n3, n4)
 	fmt.Println("sum : ", result)
+
+	a := 0
+	call_by_value(a)
+	fmt.Println("a : ", a)
+
+	call_by_ref(&a)
+	fmt.Println("a : ", a)
 }
